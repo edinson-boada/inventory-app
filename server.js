c3ba132b4290be3e9b6fd9cdab6cd3481c8cc5e3
paolo-boada-unmsm <edinson.boada@unmsm.edu.pe>
2021-11-05 17:05:48 -0500

    initial heroku deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/inventory-app'));
+app.use(express.static(__dirname + '/dist/app-angular-inventory'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/inventory-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/app-angular-inventory/index.html'));
 });
 
 // default Heroku port
